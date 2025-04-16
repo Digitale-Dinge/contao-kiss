d3b67e9fefa58904a403534fed526d7628ed546c
Philipp Travaglia <philipp@travaglia.de>
2025-04-16 13:26:13 +0200

    Updated rsce_logo
    Replaced the html5 Template with a Twig Template
    Re-factored the module config for more realistic cases

--- contao/templates/rsce/rsce_logo_config.php
+++ contao/templates/rsce/rsce_logo_config.php
@@ -4,39 +4,25 @@ return [
     'label'          => [
         'de' => [
             'Logo',
-            'Zeigt das Logo optional mit Image Replacement',
+            'Zeigt das ein Bild- oder Textlogo',
         ],
         'en' => [
             'Logo',
-            'Show logo image optional with image replacments',
+            'Displays an image or text logo',
         ],
     ],
     'types'          => [ 'module' ],
     'moduleCategory' => 'Header',
     'fields'         => [
-        'shortcut'          => [
-            'label'     => [
-                'de' => [ 'Shortcut', '' ],
-                'en' => [ 'Shortcut', '' ],
-            ],
-            'inputType' => 'group',
-        ],
-        'has_shortcut'      => [
-            'inputType' => 'standardField',
-        ],
-        'position_shortcut' => [
-            'inputType' => 'standardField',
-        ],
         'singleSRC'         => [
             'label'     => [
                 'de' => [ 'Logobild', '' ],
                 'en' => [ 'Logo image', '' ],
             ],
             'inputType' => 'standardField',
-        ],
-        'imgSize'           => [
-            'inputType' => 'standardField',
-            'eval'      => [ 'tl_class' => 'w50' ],
+            'eval' => [
+                'mandatory' => false,
+            ],
         ],
         'text'        => [
             'label'     => [
@@ -45,14 +31,6 @@ return [
             ],
             'inputType' => 'text',
             'eval'      => [ 'tl_class' => 'w50 clr' ],
-        ],
-        'imgReplace'        => [
-            'label'     => [
-                'de' => [ 'Text ersetzen', 'Den Text durch das Logobild ersetzen.' ],
-                'en' => [ 'Image replacement', 'Replace text with logo image.' ],
-            ],
-            'inputType' => 'checkbox',
-            'eval'      => [ 'tl_class' => 'w50 m12' ],
         ],
         'jumpTo'            => [
             'inputType' => 'standardField',
