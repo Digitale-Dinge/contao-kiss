90b94dd7e0868ba5dfb5c7637f69af254ced3fb7
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-12-03 16:45:42 +0100

    Load extensions within the AbstractBundle

--- src/DigitaleDingeContaoKissBundle.php
+++ src/DigitaleDingeContaoKissBundle.php
@@ -4,16 +4,14 @@ declare(strict_types=1);
 
 namespace DigitaleDinge\ContaoKiss;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-use function dirname;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-
-class DigitaleDingeContaoKissBundle extends Bundle
+class DigitaleDingeContaoKissBundle extends AbstractBundle
 {
-
-    public function getPath(): string
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        return dirname(__DIR__);
+        $container->import('../config/services.php');
     }
-
 }
