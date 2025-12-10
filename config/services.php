c6442e72174fa282605bc26f1c4837533a8ca513
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-12-10 16:14:11 +0100

    Remove the Symlinklistener

--- config/services.php
+++ config/services.php
@@ -2,10 +2,7 @@
 
 declare(strict_types=1);
 
-use DigitaleDinge\ContaoKiss\EventListener\SymlinkListener;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function(ContainerConfigurator $container)
 {
@@ -17,13 +14,5 @@ return static function(ContainerConfigurator $container)
 
         ->load('DigitaleDinge\\ContaoKiss\\', '../src/*')
             ->exclude('../src/{DigitaleDingeKissBundle.php}')
-
-        ->set(SymlinkListener::class)
-            ->args([
-                param('kernel.project_dir'),
-                param('contao.web_dir'),
-                service('filesystem'),
-                service('assets._package_digitale_dinge_contao_kiss')
-            ])
     ;
 };
