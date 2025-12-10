bbc6e468eb20e6469b3e9cc8a398f73734f82c41
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-12-10 16:13:35 +0100

    Add an opening times widget

---
+++ contao/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use DigitaleDinge\ContaoKiss\Widget\OpeningTimesTable;
+
+$GLOBALS['BE_FFL']['openingTimesTable'] = OpeningTimesTable::class;
---
+++ contao/dca/tl_page.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['opening_times'] = [
+    'inputType' => 'openingTimesTable',
+    'eval' => ['tl_class' => 'w66 clr'],
+    'sql' => "text NULL",
+];
+
+PaletteManipulator::create()
+    ->addLegend('company_data_legend', 'metadata_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('opening_times', 'company_data_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette(array_key_exists('rootfallback', $GLOBALS['TL_DCA']['tl_page']['palettes']) ? 'rootfallback' : 'root', 'tl_page')
+;
