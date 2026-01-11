1adde8d75104bba57b667974ead2ee3a78c5fb86
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-01-11 15:02:58 +0100

    Range JS

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * KISS Components - Stimulus Controllers
+ * 
+ * Import this file in your app.js and register the controllers:
+ * 
+ * import { RangeController } from '../_contao-kiss/assets/js';
+ * // or from vendor:
+ * // import { RangeController } from '../vendor/digitaledinge/contao-kiss/assets/js';
+ * 
+ * application.register('range', RangeController);
+ */
+
+export { default as RangeController } from './controllers/range-controller';
