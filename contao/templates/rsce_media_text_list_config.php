986e8eb69fc84dc451fee522ced6cdd9de8ab33a
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-05-18 11:24:12 +0200

    feat: Media text list added spacing to media feat

--- contao/templates/rsce_media_text_list_config.php
+++ contao/templates/rsce_media_text_list_config.php
@@ -15,6 +15,25 @@ return $configBuilder
     ->addDependsOnField('type', ['image', 'icon', 'separated'])
     ->addImageSizeField([], 'type')
     ->addTextAlignmentField()
+    ->addField('mediaMargin', [
+        'label' => [
+            $translator->trans('rsce.media_text_list.mediaMargin.label', [], 'rsce'),
+            $translator->trans('rsce.media_text_list.mediaMargin.description', [], 'rsce'),
+        ],
+        'inputType' => 'select',
+        'options' => array_reduce(\DigitaleDinge\ContaoKiss\Styles\Option\Margin\Bottom::cases(), static function ($carry, $case) use ($translator) {
+            $carry[$case->value] = $translator->trans($case->label()->getMessage(), [], $case->label()->getDomain());
+            return $carry;
+        }, []),
+        'eval' => [
+            'includeBlankOption' => true,
+            'tl_class' => 'w50',
+            'dependsOn' => [
+                'field' => 'type',
+                'value' => ['image', 'icon'],
+            ],
+        ],
+    ])
     ->startList()
         ->addIconField([], '../type')
         ->addImageField([], '../type')
