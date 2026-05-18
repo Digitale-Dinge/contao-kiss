1394eb066693504209f0d3bda87f07493075d19a
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-05-18 11:47:04 +0200

    fix:translation error form merge

--- contao/templates/rsce_media_text_list_config.php
+++ contao/templates/rsce_media_text_list_config.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Contao\System;
 
 $configBuilder = System::getContainer()->get('kiss.rsce_config.builder');
+$translator = System::getContainer()->get('translator');
 
 return $configBuilder
     ->create('media_text_list', 'media', [
