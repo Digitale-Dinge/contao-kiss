0fe9a7485fb8e12ce77e4b74cb572708c61fd13d
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-06-16 17:51:56 +0200

    feat: Element group ratio slider (v1)

---
+++ contao/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+use DigitaleDinge\ContaoKiss\Widget\Backend\KissGridRatio;
+
+// Reactive column-ratio ruler used by the element_group "gridRatio" field.
+$GLOBALS['BE_FFL']['kissGridRatio'] = KissGridRatio::class;
