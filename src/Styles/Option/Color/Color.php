81345086f8a61632e3c3704878300b77609d16e7
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-08-28 12:43:20 +0200

    refractor: removed Todo and smelly code comment

--- src/Styles/Option/Color/Color.php
+++ src/Styles/Option/Color/Color.php
@@ -9,9 +9,6 @@ use Symfony\Component\Translation\TranslatableMessage;
 
 enum Color: string implements TranslatableLabelInterface
 {
-    // The values are component modifiers (btn-primary, range-tertiary)
-    // ToDo: "accent" was renamed to "tertiary" without a migration. Content from
-    // older projects that stored "accent" resolves to nothing and loses its colour.
     case primary = 'primary';
     case secondary = 'secondary';
     case tertiary = 'tertiary';
