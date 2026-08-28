d94e6ee5c9776c5d88b7c95c29a376f3d3cbb845
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-08-28 15:22:35 +0200

    Migrate `accent` color to `tertiary` (see #11)
    * refactor: accent component rename to tertiary
    
    * refactor: remove info from translations
    
    * refactor: rename accent color option to tertiary
    
    * refactor: accent to tertiary
    
    * refractor: removed Todo and smelly code comment

--- src/Styles/Option/Color/Color.php
+++ src/Styles/Option/Color/Color.php
@@ -9,18 +9,12 @@ use Symfony\Component\Translation\TranslatableMessage;
 
 enum Color: string implements TranslatableLabelInterface
 {
-    // ToDo: currently used as modifier options for components -> migrate to design tokens?
     case primary = 'primary';
     case secondary = 'secondary';
-    case accent = 'accent';
+    case tertiary = 'tertiary';
     case success = 'success';
     case warning = 'warning';
     case error = 'error';
-    /*
-    case base_100 = 'base-100';
-    case base_200 = 'base-200';
-    case base_300 = 'base-300';
-    */
 
     public function label(): TranslatableMessage
     {
