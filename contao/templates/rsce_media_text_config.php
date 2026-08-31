ba82795cd49f1d0cf5822636f10e75e98bc766df
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-08-31 17:52:03 +0200

    Rework media_text and media_text_list

--- contao/templates/rsce_media_text_config.php
+++ contao/templates/rsce_media_text_config.php
@@ -7,21 +7,28 @@ use Contao\System;
 $configBuilder = System::getContainer()->get('kiss.rsce_config.builder');
 
 return $configBuilder
-    ->create('media_text', 'media', [
+    ->create('media_text', extra: [
         'types' => ['content'],
         'standardFields' => ['cssID'],
     ])
-    ->addGroup('settings')
-    ->addDependsOnField('type', ['image', 'icon', 'separated'])
-    ->addImageSizeField([], 'type')
-    ->addTextAlignmentField()
-    ->addShowAsCardField()
-    ->addIconField([], 'type')
+
+    ->addGroup('appearance')
+    ->addTextAlignmentField(eval: ['tl_class' => 'w25 clr'])
+    ->addDependsOnField('addMedia')
+    ->addDependsOnField('type', ['image', 'icon', 'video'], ['tl_class' => 'w25'], dependsOn: ['addMedia'])
+    ->addElementLayoutField(dependsOn: 'addMedia')
+    ->addImageSizeField(dependsOn: 'type')
+
     ->addGroup('media')
-    ->addImageField([], 'type')
-    ->addLinkField()
+    ->addImageField(dependsOn: 'type')
+    ->addImageUrlField(dependsOn: 'type')
+    ->addIconField(dependsOn: 'type')
+    ->addResponsiveVideoField(dependsOn: 'type')
+
     ->addHeadlineField() // Topline is appended automatically outside of lists
     ->addRichTextField()
     ->addCallToActionField()
+
+    ->addCardSettings()
     ->build()
 ;
--- contao/templates/rsce_media_text_list_config.php
+++ contao/templates/rsce_media_text_list_config.php
@@ -5,27 +5,34 @@ declare(strict_types=1);
 use Contao\System;
 
 $configBuilder = System::getContainer()->get('kiss.rsce_config.builder');
-$translator = System::getContainer()->get('translator');
 
 return $configBuilder
-    ->create('media_text_list', 'media', [
+    ->create('media_text_list', extra: [
         'types' => ['content'],
         'standardFields' => ['headline', 'topline', 'cssID'],
     ])
-    ->addGroup('settings')
-    ->addDependsOnField('type', ['image', 'icon', 'separated'])
-    ->addImageSizeField([], 'type')
-    ->addTextAlignmentField()
-    ->addShowAsCardField()
+
+    ->addGroup('appearance')
+    ->addTextAlignmentField(eval: ['tl_class' => 'w25 clr'])
+    ->addDependsOnField('addMedia')
+    ->addDependsOnField('type', ['image', 'icon'], dependsOn: ['addMedia'])
+    ->addElementLayoutField(dependsOn: 'addMedia')
+    ->addDependsOnField('type', ['image', 'icon'], dependsOn: ['addMedia'])
+    ->addImageSizeField(dependsOn: 'type')
+
     ->startList()
-        ->addIconField([], '../type')
-        ->addImageField([], '../type')
+        ->addImageField(dependsOn: '../type')
+        ->addImageUrlField(dependsOn: 'type')
+        ->addIconField(dependsOn: '../type')
         ->addHeadlineField()
         ->addToplineField()
         ->addRichTextField()
         ->addTextAppearanceField()
         ->addCallToActionField()
     ->endList()
+
+    ->addCardSettings()
     ->addGridGroup()
+
     ->build()
 ;
