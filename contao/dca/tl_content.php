89e572a452f2cfaeff63f472d7ca7a3cd80eb0dd
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-09-03 10:05:10 +0200

    Drop duplicate field from merge

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -323,7 +323,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['elementLayout'] = [
     'default' => 'default',
     'eval' => [
         'imagePath' => 'bundles/digitaledingecontaokiss/icons/media/layout/',
-        'tl_class' => 'w75',
+        'tl_class' => 'clr',
     ],
 ];
 
@@ -372,7 +372,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['responsiveVideo'] = [
     ],
     'eval' => [
         'tl_class' => 'clr',
-        'style' => 'max-width: 800px',
+        'style' => 'max-width: 980px',
         'max' => 3,
     ],
     'sql' => [
@@ -454,38 +454,3 @@ PaletteManipulator::create()
     ->addField('slidesPerView', 'sliderContinuous', PaletteManipulator::POSITION_AFTER)
     ->applyToPalette('swiper', 'tl_content')
 ;
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['responsiveVideo'] = [
-    'inputType' => 'rowWizard',
-    'fields' => [
-        'screen' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['responsiveVideoFieldScreen'],
-            'inputType' => 'select',
-            'options' => ['mobile', 'tablet', 'desktop'],
-            'reference' => &$GLOBALS['TL_LANG']['tl_content']['responsiveVideoOptions'],
-            'eval' => [
-                'cell_style' => 'width: 150px',
-            ]
-        ],
-        'video' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['responsiveVideoFieldVideo'],
-            'inputType' => 'fileTree',
-            'eval' => [
-                'multiple' => false,
-                'fieldType' => 'radio',
-                'filesOnly' => true,
-                'extensions' => 'mp4,webm',
-            ],
-        ],
-    ],
-    'eval' => [
-        'tl_class' => 'clr',
-        'style' => 'max-width: 800px',
-        'max' => 3,
-    ],
-    'sql' => [
-        'type' => 'blob',
-        'length' => MySQLPlatform::LENGTH_LIMIT_BLOB,
-        'notnull' => false
-    ]
-];
