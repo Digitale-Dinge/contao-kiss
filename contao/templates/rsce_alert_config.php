976e4476767a8ce978647073cdcfc181e90e964a
Cristian Gavriliu <cristiangavriliu2001@gmail.com>
2026-09-15 16:33:43 +0200

    Update the `alert` component and provide it as a content element (see #16)
    * New RSCE alert
    
    * Fix for wrong variables
    
    * Fix for "message" variable
    
    * fix: alert component swap backround for component color (elementColor)
    
    * feat: restrict style options to what each component implements
    
    * fix: alert kiss component icon size fix
    
    * Update to latest code style
    
    * revert: 71f8be7
    
    * refactor: alert style options as element fields, following the badge pattern
    
    * feat: added comments as instructed my skill
    
    * fix: translated comment into english
    
    * Update contao/templates/rsce_alert_config.php
    
    ---------
    
    Co-authored-by: PhilippTravaglia <philipp@travaglia.de>

---
+++ contao/templates/rsce_alert_config.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Contao\System;
+use DigitaleDinge\ContaoKiss\Styles\Option\Color\Color;
+use DigitaleDinge\ContaoKiss\Styles\Option\Modifier\Size;
+
+$configBuilder = System::getContainer()->get('kiss.rsce_config.builder');
+
+return $configBuilder
+    ->create('alert', 'texts', [
+        'types' => ['content'],
+        'standardFields' => ['cssID'],
+    ])
+
+    ->addStyleOptionsField('color', Color::class)
+    ->addDependsOnField('variant', ['', 'soft', 'outline', 'dashed'], ['tl_class' => 'w25'])
+    ->addStyleOptionsField('alertSize', Size::class)
+    ->addIconField()
+    ->addField('title', [
+        'label' => true,
+        'inputType' => 'text',
+        'eval' => [
+            'tl_class' => 'w50 clr',
+            'mandatory' => false,
+        ],
+    ])
+    ->addField('text', [
+        'label' => true,
+        'inputType' => 'textarea',
+        'eval' => [
+            'tl_class' => 'long clr',
+            'mandatory' => false,
+            'allowHtml' => true,
+        ],
+    ])
+
+    ->build()
+;
