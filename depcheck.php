56263d4dda91d790388dbc973eb305090fa9d6bd
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-09-22 16:22:15 +0200

    Update depcheck.php

--- depcheck.php
+++ depcheck.php
@@ -5,6 +5,20 @@ declare(strict_types=1);
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
+return new Configuration()
+    // These package are used for the kiss content elements
+    ->ignoreErrorsOnPackage('lukasbableck/contao-svg-icon-picker-bundle', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('madeyourday/contao-rocksolid-custom-elements', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('mvo/contao-group-widget', [ErrorType::UNUSED_DEPENDENCY])
+
+    // These packages are used within the backend UI
+    ->ignoreErrorsOnPackage('zoglo/contao-collection-widget', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('zoglo/contao-radio-image-widget', [ErrorType::UNUSED_DEPENDENCY])
+
+    // This package is required for QoL reasons
+    ->ignoreErrorsOnPackage('twig/intl-extra', [ErrorType::UNUSED_DEPENDENCY])
+
+    // The manager plugin is a dev dependency because it is only required in the
+    // managed edition.
     ->ignoreErrorsOnPackage('contao/manager-plugin', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
