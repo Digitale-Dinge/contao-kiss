da2988f4850a75dec487a317d391a15b2e5383a1
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-09-22 14:17:00 +0200

    Move `package.json` and CS tools into `/build`

--- build/stylelint.config.js
+++ build/stylelint.config.js
@@ -4,9 +4,11 @@
 const stylelintConfig = {
     extends: [
         "stylelint-config-standard",
+        "@dreamsicle.io/stylelint-config-tailwindcss",
     ],
     rules: {
         "at-rule-no-unknown": null,
+        "at-rule-prelude-no-invalid": [true, { ignoreAtRules: ["source"] }],
         "color-no-invalid-hex": true,
         "import-notation": null,
         "no-invalid-double-slash-comments": null,
