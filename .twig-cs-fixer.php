5cf695d961038317f34f48c4609d3bafc99fffbf
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-09-24 09:24:49 +0200

    Add a fixer for HtmlAttributeVariableNames

--- .twig-cs-fixer.php
+++ .twig-cs-fixer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Contao\CoreBundle\Twig\Defer\DeferTokenParser;
 use Contao\CoreBundle\Twig\ResponseContext\AddTokenParser;
 use Contao\CoreBundle\Twig\Slots\SlotTokenParser;
+use DigitaleDinge\ContaoKiss\Tools\TwigCsFixer\Rules\HtmlAttributesVariableNameRule;
 use DigitaleDinge\ContaoKiss\Tools\TwigCsFixer\Rules\ImportSelfRule;
 use DigitaleDinge\ContaoKiss\Tools\TwigCsFixer\Rules\SetUnderscoreRule;
 use DigitaleDinge\ContaoKiss\Tools\TwigCsFixer\Rules\VariableNameRule;
@@ -30,12 +31,15 @@ $ruleset->addStandard(new TwigCsFixer());
 $ruleset->overrideRule(new CompactHashRule(true));
 $ruleset->removeRule(UpstreamVariableNameRule::class);
 $ruleset->addRule(new VariableNameRule(optionalPrefix: '_', ignore: ['wrapperAttributes', 'bodyAttributes', 'cssID']));
+
 $ruleset->addRule(new SetUnderscoreRule());
 $ruleset->addRule(new ImportSelfRule());
 
 $ruleset->addRule(new DirectoryNameRule(baseDirectory: $templatePath));
 $ruleset->addRule(new FileNameRule(baseDirectory: $templatePath, optionalPrefix: '_'));
+
 $ruleset->addRule(new FileExtensionRule());
+$ruleset->addRule(new HtmlAttributesVariableNameRule(ignore: ['headline_classes', 'wrapperAttributes', 'bodyAttributes']));
 $ruleset->addRule(new ValidConstantFunctionRule());
 
 $ruleset->addRule(new ForbiddenFunctionRule([
